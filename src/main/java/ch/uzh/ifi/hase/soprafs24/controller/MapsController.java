5ead061797f9c9279542d7bc2ea5865c5c3e72ad
kiran <kiransai.nanduri@uzh.ch>
2025-03-28 11:28:08 +0100

    more mapscontroller fixing

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/MapsController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/MapsController.java
@@ -5,16 +5,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+import java.util.Collections;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 public class MapsController {
 
     @Value("${google.maps.api-key}") 
     private String apiKey;
 
     @GetMapping("/api/maps/key")
-    public String getApiKey() {
-        return apiKey;
+    public Map<String, String> getApiKey() {
+    return Collections.singletonMap("apiKey", apiKey);
     }
 }
